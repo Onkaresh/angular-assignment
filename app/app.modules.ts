54c46f02e599cd7b00909dfafdcd36dc41c34d68
Onkaresh <gonkaresh@gmail.com>
2018-02-22 12:06:09 +0530

    Update app.modules.ts
    commited

--- app/app.modules.ts
+++ app/app.modules.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './component.app';
 import { ArtistItemComponent } from './component.artist-item';
 import { ArtistDetailsComponent } from './component.artist-details';
